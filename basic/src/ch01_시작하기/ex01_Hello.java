2c223c9526dcc28b1d7153cae46f3c742965b74b
woojin0123 <iamwjwj@gmail.com>
2025-12-08 18:04:42 +0900

    251208

---
+++ basic/src/ch01_시작하기/ex01_Hello.java
@@ -0,0 +1,11 @@
+package ch01_시작하기;
+
+public class ex01_Hello {
+
+	public static void main(String[] args) {
+		// ctrl+space: 자동 완성
+		// ctrl+f11: 실행
+		System.out.println("Hi, Java");
+	}
+
+}
---
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec09;
+/**
+ * 문서화 주석
+ */
+public class Hello {
+	/* 여러줄 주석 */
+	public static void main(String[] args) {
+		// javascript 주석이랑 똑같음
+		// document.write("Hello, JavaSxript");
+		System.out.println("Hello, Java");
+	}
+}
