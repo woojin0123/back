7229b0c73a74a1c436f235c1381b7d62f21cb56e
woojin0123 <iamwjwj@gmail.com>
2025-12-12 09:19:52 +0900

    1212

--- basic/src/ch02_데이터와변수/Ex10_타입변환.java
+++ basic/src/ch02_데이터와변수/Ex10_타입변환.java
@@ -11,53 +11,63 @@ package ch02_데이터와변수;
  * 2. 강제 타입 변환
  * 		- 큰 허용 범위 타입을 작은 허용 범위 타입으로 쪼개어서 저장하는 것
  * 		- 형식:
- * 				작은타입 변수 = (작은타입) 변수
+ * 				타입 변수 = (타입) 변수
+ * 
+ * 3. 연산식에서 자동 타입 변환 
+ * 		- byte, short, char는 연산 시 int로 변환되어 연산된다.
+ * 		- int 보다 큰 타입의 경우 연산에 사용된 가장 큰 타입으로 변환되어 연산된다.
  */
 public class Ex10_타입변환 {
 
 	public static void main(String[] args) {
-		// 자동 타입 변환
+		
+		// 자동 타입(형) 변환
+		// byte -> int
 		byte byteValue = 10;
 		int intValue = byteValue;
 		System.out.println("intValue: " + intValue);
-		
+
+		// char -> int
 		char charValue = '가';
 		intValue = charValue;
-		System.out.println("'가'의 유니코드: " + charValue);
+		System.out.println("'가'의 유니코드: " + intValue);
 		
+		// int -> long
 		intValue = 50;
 		long longValue = intValue;
 		System.out.println("longValue: " + longValue);
 		
+		// long -> float
 		longValue = 100;
 		float floatValue = longValue;
 		System.out.println("floatValue: " + floatValue);
 		
+		// float -> double
 		floatValue = 100.5f;
 		double doubleValue = floatValue;
 		System.out.println("doubleValue: " + doubleValue);
 		
 		// 강제 타입 변환
-		
-		// int > byte
+		// int -> byte
 		int var1 = 10;
 		// 타입 변수명 = (타입) 변수명;
+		// byte 타입으로 바꾸어 할당한다.
 		byte var2 = (byte) var1;
 		System.out.println("var2: " + var2);
 		
-		// long > int
+		// long -> int
 		long var3 = 300;
-		int var4 = (int) var3;
-		System.out.println("var3: " + var3);
+		var1 = (int) var3;
+		System.out.println("var1: " + var1);
 		
-		// int > char
+		// int -> char
 		var1 = 65;
 		char var5 = (char) var1;
 		System.out.println("var5: " + var5);
 		
-		// double > int
-		// 정수 부분만 출력
-		double var6 = 3.14;
+		// double -> int
+		// 주의! 정수 부분만 출력된다.
+		double var6 = 3.84;
 		var1 = (int) var6;
 		System.out.println("var1: " + var1);
 		
@@ -65,34 +75,46 @@ public class Ex10_타입변환 {
 		byte result1 = 10 + 20;
 		System.out.println("result1: " + result1);
 		
+		// int 타입으로 변환 후 연산, 그리고 할당된다.
+		// byte + byte -> int
 		byte v1 = 10;
 		byte v2 = 20;
 		int result2 = v1 + v2;
 		System.out.println("result2: " + result2);
-		
+
 		byte v3 = 10;
 		int v4 = 100;
-		// long 타입 리터럴은 끝에 1또는 L을 쓴다
+		// long 타입 리터럴은 끝에 l 또는 L을 쓴다.
 		long v5 = 1000L;
+		// long 타입으로 변환되어 연산 수행 후 할당된다.
 		long result3 = v3 + v4 + v5;
 		System.out.println("result3: " + result3);
 		
-		// int 타입보다 작은 타입의 연산은 int 타입으로 변환되어 연산된다.
-		char v6 = 'A'; // int 타입에서 'A' > 65
-		char v7 = '1'; // int 타입에서 '1' > 49로 할당 
+		// int 타입 보다 작은 타입의 연산은 int 타입으로 변환되어 연산된다.
+		char v6 = 'A'; // int 타입에서 'A' -> 65로 할당
+		char v7 = '1'; // int 타입에서 '1' -> 49로 할당
 		int result4 = v6 + v7;
 		System.out.println("result4: " + result4);
 		
+		// 정수 연산의 결과는 정수이다.
 		int v8 = 10;
-		int result5 = v8 / 4;
+		int result5 = v8 / 4; // 2
 		System.out.println("result5: " + result5);
+
+		// 연산 전 double 타입으로 변환되어 연산된다.
+		double result6 =  v8 / 4.0; // 2.5
+		System.out.println("result6: " + result6);
 		
-		// 연산 전 double 타입으로 변환되어 연산
-		double result5 = (int) v8 / 4.0;
-		System.out.println("result5: " + result5);
-		
-		
-		
+		int v10 = 1;
+		int v11 = 2;
+		double result7 = (double) v10 / v11; // 0.5
+		System.out.println("result7: " + result7);
 	}
+	
+	
+	
+	
+	
+	
 
 }
--- basic/src/ch02_데이터와변수/Ex13_콘솔출력.java
+++ basic/src/ch02_데이터와변수/Ex13_콘솔출력.java
@@ -1,21 +1,47 @@
 package ch02_데이터와변수;
 
 public class Ex13_콘솔출력 {
-
 	public static void main(String[] args) {
 		int value = 123;
-		// 줄바꿈 된다
-		System.out.println(value);
-		// 줄바꿈 안된다
-		System.out.print(value);
 		
-		// format 지정 출력
-		// System.out.printf("형식문자", 출력값);
+		// 줄바꿈 된다.
+		System.out.println(value);
+		// 줄바꿈 안된다.
+		System.out.print(value + "\n");
+		
+		// format(형식) 지정 출력
+		/* 
+		 * System.out.printf("형식문자열", 값1, [값2],[...]);
+		 * 	- 형식 문자열에 맞추어 뒤의 값을 출력한다.
+		 * 	- 형식 문자열 구성
+		 * 		%[값의순번$][플래그][전체자릿수][.소수자릿수]변환문자
+		 * 		- %는 형식 문자열의 시작
+		 * 		- 변환 문자는 d(정수), f(실수), s(문자열), c(문자), b(불리언)
+		 * 		- 플래그는 
+		 * 			생략하면 왼쪽이 공백으로 채워지고,
+		 * 			-는 오른쪽이 공백으로 채워지고,
+		 * 			0은 왼쪽이 공백 대신 0으로 채워진다.
+		 *   - 줄 바꿈은 \n 또는 %n을 직접 넣는다.
+		 */
+		
 		System.out.printf("상품 가격: %d원\n", value);
 		System.out.printf("상품 가격: %6d원\n", value);
 		System.out.printf("상품 가격: %-6d원\n", value);
 		System.out.printf("상품 가격: %06d원\n", value);
-
+		
+		double area = 3.14159 * 10 * 10;
+		// 형식 문자열의 개수 만큼 값을 표시해야 한다.
+		System.out.printf("반지름이 %d인 원의 넓이는 %10.2f 입니다.\n", 10, area);
+		
+		String name = "홍길동";
+		String job = "개발자";
+		System.out.printf("%06d | %-10s | %10s\n", 1, name, job);
+		
 	}
-
+	
+	
+	
+	
+	
+	
 }
